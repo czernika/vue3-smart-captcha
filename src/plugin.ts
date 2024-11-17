836efee6a3215ce0442f2cbad42f12bcaaeac8c7
Aliakseyenka Ihar <aliha.devs@gmail.com>
2024-11-17 21:59:31 +0300

    fix: update version to rc

--- src/plugin.ts
+++ src/plugin.ts
@@ -1,12 +1,11 @@
-import type { Plugin } from 'vue'
 import SmartCaptcha from '@/components/SmartCaptcha.vue'
 import { useSmartCaptcha } from '@/composables/useSmartCaptcha'
+import type { Plugin } from 'vue'
 import type {
     SmartCaptcha as WindowSmartCaptcha,
     WidgetId,
     Token,
 } from '@/types/smartcaptcha'
-
 
 const SmartCaptchaPlugin: Plugin = {
     install: (app) => {
